91d084373fc3af99f41c2ac36b00adfcfaa73d82
Michael Samorokov <michael.samorokov@gmail.com>
2019-02-10 20:02:10 -0600

    Added an example how to write from Serilog to log4net

--- Log4NetSplunk.Web/Controllers/HomeController.cs
+++ Log4NetSplunk.Web/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using log4net;
 using Newtonsoft.Json;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,27 +12,40 @@ namespace Log4NetSplunk.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILog log = LogManager.GetLogger(nameof(HomeController));
+        private Serilog.Core.Logger serilog = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .Enrich.WithHttpRequestClientHostIP()
+                .Enrich.WithHttpRequestId()
+                .Enrich.WithHttpRequestType()
+                .Enrich.WithMvcControllerName()
+                .Enrich.WithMvcActionName()
+                .MinimumLevel.Verbose()
+                .WriteTo.Log4Net()
+                .CreateLogger();
 
         public ActionResult Index()
         {
-            log.Info("Index");
+            log.Info($"{nameof(Index)}");
             log.Debug(JsonConvert.SerializeObject(Request.Headers));
+            serilog.Debug("{@Headers}", Request.Headers);
             return View();
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-            log.Info("About");
+            log.Info($"{nameof(About)}");
             log.Debug(JsonConvert.SerializeObject(Request.Headers));
+            serilog.Debug("{@Headers}", Request.Headers);
             return View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-            log.Info("Contact");
+            log.Info($"{nameof(Contact)}");
             log.Debug(JsonConvert.SerializeObject(Request.Headers));
+            serilog.Debug("{@Headers}", Request.Headers);
             return View();
         }
     }
